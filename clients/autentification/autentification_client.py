047f86c8211f1b5be84e668f645f4c8d9464e97c
vyacheslav758 <chubacka758@gmail.com>
2026-08-07 16:19:47 +0300

    Add method get auth client

--- clients/autentification/autentification_client.py
+++ clients/autentification/autentification_client.py
@@ -3,6 +3,7 @@ from typing import TypedDict
 from httpx import Response
 
 from clients.api_client import APIClient
+from clients.public_http_builder import get_public_http_client  # Импортируем builder
 
 
 class LoginRequestDict(TypedDict):
@@ -44,3 +45,11 @@ class AuthenticationClient(APIClient):
         return self.post("/api/v1/authentication/refresh", json=request)
 
 
+# Добавляем builder для AuthenticationClient
+def get_authentication_client() -> AuthenticationClient:
+    """
+    Функция создаёт экземпляр AuthenticationClient с уже настроенным HTTP-клиентом.
+
+    :return: Готовый к использованию AuthenticationClient.
+    """
+    return AuthenticationClient(client=get_public_http_client())
