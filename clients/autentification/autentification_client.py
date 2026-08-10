68a4eeef5785e7c5cc976e9b2b6f2d882b6b0110
vyacheslav758 <chubacka758@gmail.com>
2026-08-10 12:41:29 +0300

    Add method login for return response.json

--- clients/autentification/autentification_client.py
+++ clients/autentification/autentification_client.py
@@ -3,7 +3,16 @@ from typing import TypedDict
 from httpx import Response
 
 from clients.api_client import APIClient
-from clients.public_http_builder import get_public_http_client  # Импортируем builder
+from clients.public_http_builder import get_public_http_client
+
+
+class Token(TypedDict):  # Добавили структуру с токенами аутентификации
+    """
+    Описание структуры аутентификационных токенов.
+    """
+    tokenType: str
+    accessToken: str
+    refreshToken: str
 
 
 class LoginRequestDict(TypedDict):
@@ -12,6 +21,13 @@ class LoginRequestDict(TypedDict):
     """
     email: str
     password: str
+
+
+class LoginResponseDict(TypedDict):  # Добавили структуру ответа аутентификации
+    """
+    Описание структуры ответа аутентификации.
+    """
+    token: Token
 
 
 class RefreshRequestDict(TypedDict):
@@ -44,8 +60,12 @@ class AuthenticationClient(APIClient):
         """
         return self.post("/api/v1/authentication/refresh", json=request)
 
+    # Добавили метод login
+    def login(self, request: LoginRequestDict) -> LoginResponseDict:
+        response = self.login_api(request)  # Отправляем запрос на аутентификацию
+        return response.json()  # Извлекаем JSON из ответа
 
-# Добавляем builder для AuthenticationClient
+
 def get_authentication_client() -> AuthenticationClient:
     """
     Функция создаёт экземпляр AuthenticationClient с уже настроенным HTTP-клиентом.
